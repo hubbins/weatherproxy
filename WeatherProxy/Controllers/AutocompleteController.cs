72ae311a4422d6da2d1a5cc3ff229a5d648b4ad7
Sean OConnor <soconnor@yahoo.com>
2015-09-07 18:23:38 -0500

    added jsonp support

--- WeatherProxy/Controllers/AutocompleteController.cs
+++ WeatherProxy/Controllers/AutocompleteController.cs
@@ -10,11 +10,17 @@ namespace WeatherProxy.Controllers
 {
     public class AutocompleteController : ApiController
     {
-        public HttpResponseMessage Get(string query)
+        public HttpResponseMessage Get(string query, string callback = "")
         {
             using (WebClient client = new WebClient())
             {
                 string json = client.DownloadString("http://autocomplete.wunderground.com/aq?query=" + query);
+
+                if (!string.IsNullOrEmpty(callback))
+                {
+                    json = callback + "(" + json + ")";
+                }
+
                 var response = new HttpResponseMessage(HttpStatusCode.OK);
                 response.Content = new StringContent(json, Encoding.Unicode, "application/json");
                 return response;
