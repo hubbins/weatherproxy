14aca6bab5c8ce12a49a9c2554fd9176ded24816
Sean OConnor <soconnor@yahoo.com>
2015-09-07 17:43:29 -0500

    basic service

---
+++ WeatherProxy/Controllers/AutocompleteController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace WeatherProxy.Controllers
+{
+    public class AutocompleteController : ApiController
+    {
+        public HttpResponseMessage Get(string query)
+        {
+            using (WebClient client = new WebClient())
+            {
+                string json = client.DownloadString("http://autocomplete.wunderground.com/aq?query=" + query);
+                var response = new HttpResponseMessage(HttpStatusCode.OK);
+                response.Content = new StringContent(json, Encoding.Unicode, "application/json");
+                return response;
+            }
+        }
+    }
+}
---
+++ WeatherProxy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WeatherProxy.WebApiApplication" Language="C#" %>
